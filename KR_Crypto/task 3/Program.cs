7a63af5fe700b33c9a6112777352c28d3d6e37f2
2pa-h8 <78879018+2pa-h8@users.noreply.github.com>
2021-10-30 16:41:29 +0300

    Add files via upload

---
+++ KR_Crypto/task 3/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+using System.Text;
+
+namespace task_3
+{
+    class Program
+    {
+        static readonly char[] characters = new char[] { '#', 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И',
+                                                        'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С',
+                                                        'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ',
+                                                        'Э', 'Ю', 'Я', ' ', '1', '2', '3', '4', '5', '6', '7',
+                                                        '8', '9', '0', };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сообщение:");
+            var msg = Console.ReadLine();
+
+            Console.WriteLine("Введите 2 простых числа p и q:");
+
+            int p = Convert.ToInt32(Console.ReadLine());
+            int q = Convert.ToInt32(Console.ReadLine());
+
+            if (IsTheNumberSimple(p) && IsTheNumberSimple(q))
+            {
+                int n = p * q;
+                Console.WriteLine('\n' + "n: " + n);
+
+                int phi = (p - 1) * (q - 1); //функция Эйлера
+                Console.WriteLine("phi: " + phi);
+
+                int d = Calculate_d(phi);
+                Console.WriteLine("d: " + d);
+
+                int e = Calculate_e(d, phi);
+                Console.WriteLine("e: " + e + '\n');
+
+                Console.WriteLine("Открытый ключ: (" + e + ", " + n + ")");
+                Console.WriteLine("Закрытый ключ: (" + d + ", " + n + ")" + '\n');
+
+                List<string> result = RSA_Endoce(msg, e, n);
+
+                StringBuilder sb = new StringBuilder();
+                foreach (string item in result)
+                    sb.Append(item + " ");
+                Console.WriteLine("[ " + sb + "]");
+
+            }
+            else
+            { 
+                Console.WriteLine("p или q - не простые числа!");
+            }
+        }
+
+        //проверка: простое ли число?
+        static bool IsTheNumberSimple(int n)
+        {
+            if (n < 2)
+                return false;
+
+            if (n == 2)
+                return true;
+
+            for (long i = 2; i < n; i++)
+                if (n % i == 0)
+                    return false;
+
+            return true;
+        }
+
+        //Проверка: взаимно ли простые числа?
+        public static bool IsCoprime(int num1, int num2)
+        {
+            if (num1 == num2)
+            {
+                return num1 == 1;
+            }
+            else
+            {
+                if (num1 > num2)
+                {
+                    return IsCoprime(num1 - num2, num2);
+                }
+                else
+                {
+                    return IsCoprime(num2 - num1, num1);
+                }
+            }
+        }
+
+        //вычисление параметра d. d должно быть взаимно простым с phi
+        static int Calculate_d(int phi)
+        {
+            int d = 1;
+            
+            for (int i = 1; i < phi - 1; i++)
+            {
+                if (IsCoprime(phi, i))
+                {
+                    Console.Write("{0} ", i);
+                }
+            }
+
+
+            //for (int i = 2; i <= phi; i++)
+            //    if ((phi % i == 0) && (d % i == 0)) //если имеют общие делители
+            //  {
+            //    d++;
+            //  i = 1;
+            //}
+            d = 1051;
+            return d;
+        }
+
+        //вычисление параметра e
+        static int Calculate_e(int d, int phi)
+        {
+            int e = 10;
+
+            while (true)
+            {
+                if (((e * d) - 1) % phi == 0)
+                    break;
+                else
+                    e++;
+            }
+
+            return e;
+        }
+
+        //зашифровать
+        static List<string> RSA_Endoce(string s, long e, long n)
+        {
+            List<string> result = new List<string>();
+
+            StringBuilder sb = new StringBuilder();
+
+            BigInteger bi;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                int index = Array.IndexOf(characters, s[i]);
+
+                sb.Append(index+ " ");
+
+                bi = new BigInteger(index);
+                bi = BigInteger.Pow(bi, (int)e);
+
+                BigInteger n_ = new BigInteger((int)n);
+
+                bi = bi % n_;
+
+                result.Add(bi.ToString());
+            }
+
+            Console.WriteLine("[ " + sb + "]");
+
+            return result;
+        }
+
+    }
+}
---
+++ KR_Crypto/task2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("x = ");
+            var x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Начало диапазона: ");
+            var s = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Конец диапазона: ");
+            var e = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Взаимно простые числа к числу {0}", x);
+            for (int i = s; i < e + 1; i++)
+            {
+                if (IsCoprime(x, i))
+                {
+                    Console.Write("{0} ", i);
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        public static bool IsCoprime(int num1, int num2)
+        {
+            if (num1 == num2)
+            {
+                return num1 == 1;
+            }
+            else
+            {
+                if (num1 > num2)
+                {
+                    return IsCoprime(num1 - num2, num2);
+                }
+                else
+                {
+                    return IsCoprime(num2 - num1, num1);
+                }
+            }
+        }
+
+
+    }
+}
